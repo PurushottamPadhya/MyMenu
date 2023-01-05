b9db6a4e780a05e6746aec91e3791ca1cd10ea77
Purushottam Padhya <Purushottam.padhya@gmail.com>
2023-01-05 18:11:57 +1100

    initial commit

---
+++ MyMenu/MyMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMenuApp.swift
+//  MyMenu
+//
+//  Created by Purushottam Padhya on 5/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
